031dbcaa3fc4d4545fe9b40d028373924933177b
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-12-08 00:12:39 -0400

    Improving the functionality of the page.

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
+const getRow = document.getElementsByClassName("row");
 let bookNew = document.getElementById("newBook");
+let displayUpdate = document.getElementById("refresh");
 
 function books(title, author, pages, read) {
     this.title = title;
@@ -9,11 +11,10 @@ function books(title, author, pages, read) {
 }
 
 books.prototype.info = function() {
-    if (this.read === true) {
-        console.log("You have read " + this.title + " by " + this.author + " that had " + this.pages + " pages.");
-    } else {
-        console.log("You have not read " + this.title + " by " + this.author + " that had " + this.pages + " pages.");
-    }
+    if (this.read === true)
+        return "Title: " + this.title + " By: " + this.author + " - " + this.pages + " pages  Read";
+    else
+        return this.title + " by " + this.author + " - " + this.pages + " pages - Not Read";
 }
 
 const book1 = new books("The Hobbit", "J.R.R. Tolkien", 295, true);
@@ -27,28 +28,40 @@ myLibrary.push(book3);
 //     let title = document.getElementById("title").value;
 //     let author = document.getElementById("author").value;
 //     let pages = document.getElementById("pages").value;
-//     let read = document.getElementById("read").value;
-//     let book = new books(title, author, pages, read);
-//     myLibrary.push(book);
-//     displayBooks();
+//     let read = document.getElementById("read").checked;
+//     let newBook = new books(title, author, pages, read);
+//     myLibrary.push(newBook);
+//     render();
 // }
 
-// function displayBooks() {
-//     let table = document.getElementById("bookTable");
-//     for (let i = 0; i < myLibrary.length; i++) {
-//         let cell1 = table.insertRow(i + 1);
-//         let cell2 = table.insertCell(-1);
-//         let cell3 = table.insertCell(-1);
-//         let cell4 = table.insertCell(-1);
-//         let cell5 = table.insertCell(-1);
-//         cell1.innerHTML = myLibrary[i].title;
-//         cell2.innerHTML = myLibrary[i].author;
-//         cell3.innerHTML = myLibrary[i].pages;
-//         cell4.innerHTML = myLibrary[i].read;
-//         cell5.innerHTML = '<button id="delete" onclick="deleteRow(this)">Delete</button>';
-//     }
-// }
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookInfo = book.info();
+    //     let newcolumn = document.createElement("column");
+    //     newcolumn.className = "column";
+    //     getRow.appendChild(newcolumn);
+    //     let newDiv = document.createElement("card");
+    //     newRow.className = "card";
+    //     newRow.innerHTML = bookInfo;
+    //     newcolumn.appendChild(newRow);
+    // }
+        // let newcolumn = document.createElement("div");
+        // newcolumn.className = "column";
+        // getRow.appendChild(newcolumn);
+        let bookDiv = document.getElementsByClassName("card");
+        // newcolumn.appendChild(bookDiv);
+        let bookDivElement = document.createElement("card");
+        bookDivElement.classList.add("book");
+        bookDivElement.innerHTML = bookInfo;
+        bookDiv[i].appendChild(bookDivElement);
+    }
+}
 
 // bookNew.addEventListener("click", addBookToLibrary()); {
-    
+
 // };
+
+displayUpdate.addEventListener("click", displayBooks());{
+
+};
